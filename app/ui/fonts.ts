66bed2e7c0d0052421852b2462373a666c6a2a2b
Skaner2010 <mikolajwolfinger@gmail.com>
2024-03-22 17:27:16 +0100

    second commit

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,6 @@
-import { Inter,Actor, Sedgwick_Ave_Display } from 'next/font/google';
+import { Inter,Actor, Sedgwick_Ave_Display, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const actor = Actor({weight:'400' , subsets:['latin']});
 export const sedgwick_ave_display = Sedgwick_Ave_Display({weight:'400' , subsets:['vietnamese']})
+export const lusitana = Lusitana({weight:'700' , subsets:['latin']})
